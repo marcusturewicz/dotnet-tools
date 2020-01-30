70e80ce8e2a4cab4da3e4a91e34809550a78d1b9
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-01-30 17:50:34 +1100

    Pagination working. Need to fix styling of buttons

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import axios from 'axios'
 import Tool from '../tool/tool'
-import { FormControl, InputLabel, Input, LinearProgress } from '@material-ui/core';
+import { FormControl, InputLabel, Input, LinearProgress, TablePagination, Button } from '@material-ui/core';
 
 export default class Search extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            hasSearched: false,
-            hasData: false,
             tools: undefined,
-            loading: false
+            loading: false,
+            page: 0,
+            pageSize: 20,
+            search: ''
         }
     }
 
     render() {
         const handleKeyPress = (event) => {
             if (event.key === 'Enter') {
-                this.setState({ loading: true });
-                const q = event.target.value;
-                axios.get(`https://azuresearch-usnc.nuget.org/query?q=${q}&packageType=DotnetTool&skip=0&take=20`).then(response => {
-                    this.setState({ hasSearched: true })
-                    if (response.data.data.length > 0)
-                        this.setState({ hasData: true })
-                    this.setState({ tools: response.data.data });
-                    this.setState({ loading: false });
-                });
+                this.setState({page: 0, search: event.target.value}, () => searchNuget() );
             }
+        }
+        const searchNuget = () => {
+            this.setState({ loading: true }, () => {
+                const pageSkip = this.state.page * this.state.pageSize;
+                axios.get(`https://azuresearch-usnc.nuget.org/query?q=${this.state.search}&packageType=DotnetTool&skip=${pageSkip}&take=${this.state.pageSize}`).then(response => {
+                    this.setState({ tools: response.data.data, loading: false });
+                });
+            });
+        }
+        const onPrevClicked = () => {
+            this.setState({ page: this.state.page - 1 }, () => searchNuget());
+        }        
+        const onNextClicked = () => {
+            this.setState({ page: this.state.page + 1 }, () => searchNuget());
         }
         return (
             <div>
@@ -37,14 +44,21 @@ export default class Search extends React.Component {
                         id="search-bar"
                         onKeyDown={handleKeyPress}
                     />
-                </FormControl>
+                </FormControl>            
                 {this.state.loading && <LinearProgress />}
+                {
+                    this.state.tools && this.state.tools.length > 0 &&
+                    <div >
+                        <Button disabled={!this.state.page} variant="contained" onClick={onPrevClicked}>Previous</Button>
+                        <Button disabled={this.state.tools.length < this.state.pageSize} variant="contained" onClick={onNextClicked}>Next</Button>
+                    </div>
+                }                    
                 {this.state.tools && this.state.tools.length > 0 &&
                     <div>
                         <br />
                         {this.state.tools.map(data => <div><Tool key={data.id} value={data}></Tool><br /></div>)}
                     </div>}
-                {this.state.tools && this.state.tools.length === 0 && <p hidden={!this.state.tools}>0 results could be found.</p>}
+                {this.state.tools && this.state.tools.length === 0 && <p hidden={!this.state.tools}>No tools could be found.</p>}
             </div>
         )
     }
